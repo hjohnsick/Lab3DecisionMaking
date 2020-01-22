abd73ba9b0265e28a6e354f08832046edd7a2ca1
heather <hjohnsick@yahoo.com>
2020-01-22 16:51:50 -0500

    Lab3DecisionMaking

---
+++ Lab3DecisionMaking/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Lab3DecisionMaking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+            string userName = Console.ReadLine();
+            
+            Console.Write($"Hi {userName}!\nEnter a number between 1 and 100: ");
+            int userInput = int.Parse(Console.ReadLine());
+
+
+            if (userInput % 2 != 0)
+            {
+                Console.WriteLine($"{userInput} is Odd.");
+            }
+            else if ((userInput % 2 == 0) && (userInput <= 25))
+            {
+                Console.WriteLine("Even and less than 25.");
+            }
+            else if ((userInput % 2 == 0) && (userInput <= 60))
+            {
+                Console.WriteLine("Even");
+            }
+            else if ((userInput%2 == 0) && (userInput > 60))
+            {
+                Console.WriteLine($"{userInput} is even.");
+            }
+            else if ((userInput % 2 != 0) && (userInput >= 60))
+            {
+                Console.WriteLine($"{userInput} is odd.");
+            }
+
+
+            Console.WriteLine($"\nThank you for entering your numbers, {userName}!  The end.");
+            Console.ReadKey();
+        }
+
+        
+
+    }
+}
